80be83c208e09135000e4132a20359bc359f3e44
Timmy-Pierre Keo <timmypierre.keo@gmail.com>
2019-09-06 22:09:13 -0400

    init

---
+++ UI/ZZZFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PackReader
+{
+    public class ZZZFile
+    {
+        public readonly string Path;
+        public List<string> FileNames;
+        public List<int> Sizes;
+        public List<int> Pointers;
+        public readonly int BaseLength;
+        public int NumberOfFiles;
+
+        public ZZZFile(string Path, int BaseLength)
+        {
+            this.Path = Path;
+            FileNames = new List<string>();
+            Sizes = new List<int>();
+            Pointers = new List<int>();
+            this.BaseLength = BaseLength;
+            this.NumberOfFiles = 0;
+        }
+    }
+}
